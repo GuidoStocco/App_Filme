4de12358d1b4abcf3f54e1bcda223d6c25f1d047
unknown <guidostoccodev@gmail.com>
2024-09-17 19:05:47 -0300

    Estilizando o card de filmes

--- src/Filmes/Filmes.js
+++ src/Filmes/Filmes.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 
 
 export default function Filmes({data}){
     return(
         <View>
-            <Text>{data.nome}</Text>
+
+            <View style={styles.card}>
+                <Text style={styles.titulo}>{data.nome}</Text>
+
+                <Image 
+                source={{uri: data.foto}}
+                style={styles.foto}
+                />
+            </View> 
+
         </View>
     );
 };
+
+
+const styles = StyleSheet.create({
+    card:{
+        backgroundColor: '#fff',
+        elevation: 2,
+        margin: 15
+    },
+    titulo:{
+
+    },
+    foto:{
+
+    }
+})
